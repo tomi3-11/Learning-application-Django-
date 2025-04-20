3ca3445d3a8624465652c7d4843c8cdf038124dd
thomas <didymusthomas6@gmail.com>
2025-04-20 16:02:31 +0300

    Ready for deployment to heroku

---
+++ Learning_logs/urls.py
@@ -0,0 +1,23 @@
+"""Define URL patterns for Learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'Learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Show all topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Topic inputs 
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # New entry 
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Edit entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    # Delete entry
+    
+]
